adae5f04887462cc0af870b93759d2a2189cfe1a
ajschmaltz <drew@mainstreetmower.com>
2014-06-10 13:14:02 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,35 @@ var gulp = require('gulp');
 var minify = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var prefix = require('gulp-autoprefixer');
+var smhtml = require('gulp-htmlmin')
 
 
 gulp.task('css', function() {
-  return gulp.src('public/source/*.css')
+  return gulp.src('source/css/**')
  .pipe(prefix("last 5 version"))
  .pipe(minify())
  .pipe(concat('all.css'))
  .pipe(gulp.dest('public/css'));
 });
 
+gulp.task('html', function(){
+  return gulp.src('source/views/**')
+  .pipe(gulp.dest('app/views'));
+});
+
+gulp.task('js', function(){
+  return gulp.src('source/js/**')
+  .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('default', function() {
   gulp.run('css');
+  gulp.run('html');
+  gulp.run('js');
   
-  gulp.watch('public/source/*.css', function(){
+  gulp.watch('source/**', function(){
     gulp.run('css');
+    gulp.run('html');
+    gulp.run('js');
   });
 });
