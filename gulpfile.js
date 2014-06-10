339084fa2eefabf1409b64b15955a1bde964a3d1
ajschmaltz <drew@mainstreetmower.com>
2014-06-10 10:14:19 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+gulp.task('css', function() {
+  return gulp.src('/public/source/css/*.css')
+ .pipe(uglify())
+ .pipe(concat('all.css'))
+ .pipe(gulp.dest('/public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('/public/source/css/*.css', ['css']);
 });
