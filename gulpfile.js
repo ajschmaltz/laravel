e9b608540067d89a2cb2c674597544dd51fb23e3
ajschmaltz <drew@mainstreetmower.com>
2014-06-10 11:07:10 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 var prefix = require('gulp-autoprefixer');
 
 
-gulp.task('css', function() {
+gulp.task('default', function() {
   return gulp.src('public/source/*.css')
  .pipe(prefix("last 5 version"))
  .pipe(minify())
